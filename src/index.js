27acaef34597bd541435c41a4c5c5e739dadbcff
Mamadoubarry10 <Mamadou.barry1105@gmail.com>
2020-10-06 15:17:05 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const task = document.getElementById('new-task-description');
+  const list = document.getElementById('tasks');
+  const button = document.querySelector('input[type=submit]')
+  
+  button.addEventListener('click', function(e){         
+    e.preventDefault()
+    // console.log(task.value)
+    let listItem = document.createElement('li');
+    listItem.textContent = task.value
+    list.append(listItem);
+
+    task.value = ''
+  });
+
+
 });
